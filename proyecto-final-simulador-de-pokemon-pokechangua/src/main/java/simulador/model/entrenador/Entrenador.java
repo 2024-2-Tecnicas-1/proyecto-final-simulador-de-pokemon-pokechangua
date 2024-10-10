f1f0e33c1b510682107e2cd2ba3acec71d4e6cbf
13rian Vargas <bsvargasc13@gmail.com>
2024-10-10 18:15:35 -0500

    IN DEV - Clase Entrenador
    - Agregro carpintería.
    - Agrego método agregarPokemon.

--- proyecto-final-simulador-de-pokemon-pokechangua/src/main/java/simulador/model/entrenador/Entrenador.java
+++ proyecto-final-simulador-de-pokemon-pokechangua/src/main/java/simulador/model/entrenador/Entrenador.java
@@ -1,5 +1,34 @@
 package simulador.model.entrenador;
 
-public class Entrenador {
-    // TODO: Aquí va tu código
+import java.io.Serializable;
+import java.util.LinkedList;
+
+import simulador.model.pokemon.Pokemon;
+
+public class Entrenador implements Serializable{
+    private String nombre;
+    private LinkedList<Pokemon> pokeLista;
+    
+    public Entrenador(String nombre, LinkedList<Pokemon> pokeLista) {
+        this.nombre = nombre;
+        this.pokeLista = pokeLista;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LinkedList<Pokemon> getPokeLista() {
+        return pokeLista;
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante
+    public void agregarPokemon(Pokemon pokemon){
+        pokeLista.add(pokemon);
+    }
+
 }
