f4f10f1bf90c7fcea5ce9be9459547397bd14b2f
13rian Vargas <bsvargasc13@gmail.com>
2024-10-10 03:36:17 -0500

    IN DEV - Avance de ventana.
    - Agrego 3 menús.
    - Agrego métodos mostrarMensaje() y espacioVisual().

--- proyecto-final-simulador-de-pokemon-pokechangua/src/main/java/simulador/view/Vista.java
+++ proyecto-final-simulador-de-pokemon-pokechangua/src/main/java/simulador/view/Vista.java
@@ -5,13 +5,93 @@ import java.util.Scanner;
 public class Vista {
     private Scanner sc = new Scanner(System.in);
     
-    public static void mostrarMensaje(String mensaje){
-        System.out.println(mensaje);
-    }
-    
-    public static String pedirString(){
+    public static String pedirString(String info){
     	Vista v = new Vista();
-        return v.sc.nextLine();
+        System.out.println(" >> " + info + ": ");
+        String dato = v.sc.nextLine();
+        return dato;
+    }
+
+    // > > > > > > > > > > > > > > > > > > > > - < < < < < < < < < < < < < < < < < < < < //
+    // > > > > > > > > > > > > > > > > > > M E N Ú S < < < < < < < < < < < < < < < < < < //
+    // > > > > > > > > > > > > > > > > > > > > - < < < < < < < < < < < < < < < < < < < < //
+
+    public static void menuPrincipal(){
+        System.out.println(
+                        "       ,___          .-;'   |  | » » »     MENÚ PRINCIPAL     « « « |  |   ';-.          ___,\n" + //
+                        "       `\"-.`\\_...._/`.`     |  |                                    |  |    `.`\\_...._/`.-\"`\n" + //
+                        "    ,      \\        /       |  |                                    |  |      \\        /      ,\n" + //
+                        " .-' ',    / ()   ()\\       |  |  [1] | Gestionar entrenadores.     |  |      /()   () \\    .' `-.\n" + //
+                        "`'._   \\  /()   .   (|      |  |                                    |  |     |)   .   ()\\  /   _.'`\n" + //
+                        "    > .' ;,    -'-  /       |  |  [2] | Gestionar pokemones.        |  |      \\  -'-    ,; '. <\n" + //
+                        "   / <   |;,     __.;       |  |                                    |  |      ;.__     ,;|   > \\\n" + //
+                        "   '-.'-.|  , \\    , \\      |  |  [3] | Iniciar batalla.            |  |     / ,    / ,  |.-'.-'\n" + //
+                        "      `>.|;, \\_)    \\_)     |  |                                    |  |    (_/    (_/ ,;|.<'\n" + //
+                        "       `-;     ,    /       |  |  [0] | Salir.                      |  |       \\    ,     ;-'\n" + //
+                        "          \\    /   <        |  |                                    |  |        >   \\    /\n" + //
+                        "           '. <`'-._)       |  |                                    |  |       (_,-'`> .'\n" + //
+                        "            '._)            |  | » » »      PokeChangua       « « « |  |           (_,'\n" + //
+                        "");
+    }
+
+    public static void menuGestionarEntrenadores(){
+        System.out.println(
+                        "       ,___          .-;'   |  | » » » GESTIONAR ENTRENADORES « « « |  |   ';-.          ___,\n" + //
+                        "       `\"-.`\\_...._/`.`     |  |                                    |  |    `.`\\_...._/`.-\"`\n" + //
+                        "    ,      \\        /       |  |  [1] | Registrar nuevo entrenador. |  |      \\        /      ,\n" + //
+                        " .-' ',    / ()   ()\\       |  |                                    |  |      /()   () \\    .' `-.\n" + //
+                        "`'._   \\  /()   .   (|      |  |  [2] | Ver lista de entrenadores.  |  |     |)   .   ()\\  /   _.'`\n" + //
+                        "    > .' ;,    -'-  /       |  |                                    |  |      \\  -'-    ,; '. <\n" + //
+                        "   / <   |;,     __.;       |  |  [3] | Seleccionar un entrenador.  |  |      ;.__     ,;|   > \\\n" + //
+                        "   '-.'-.|  , \\    , \\      |  |                                    |  |     / ,    / ,  |.-'.-'\n" + //
+                        "      `>.|;, \\_)    \\_)     |  |  [4] | Volver al menú principal.   |  |    (_/    (_/ ,;|.<'\n" + //
+                        "       `-;     ,    /       |  |                                    |  |       \\    ,     ;-'\n" + //
+                        "          \\    /   <        |  |  [0] | Salir.                      |  |        >   \\    /\n" + //
+                        "           '. <`'-._)       |  |                                    |  |       (_,-'`> .'\n" + //
+                        "            '._)            |  | » » »      PokeChangua       « « « |  |           (_,'\n" + //
+                        "");
+    }
+
+    public static void menuGestionarPokemones(){
+        System.out.println(
+                        "       ,___          .-;'   |  | » » » GESTIONAR ENTRENADORES « « « |  |   ';-.          ___,\n" + //
+                        "       `\"-.`\\_...._/`.`     |  |                                    |  |    `.`\\_...._/`.-\"`\n" + //
+                        "    ,      \\        /       |  |  [1] | Registrar nuevo entrenador. |  |      \\        /      ,\n" + //
+                        " .-' ',    / ()   ()\\       |  |                                    |  |      /()   () \\    .' `-.\n" + //
+                        "`'._   \\  /()   .   (|      |  |  [2] | Ver lista de entrenadores.  |  |     |)   .   ()\\  /   _.'`\n" + //
+                        "    > .' ;,    -'-  /       |  |                                    |  |      \\  -'-    ,; '. <\n" + //
+                        "   / <   |;,     __.;       |  |  [3] | Seleccionar un entrenador.  |  |      ;.__     ,;|   > \\\n" + //
+                        "   '-.'-.|  , \\    , \\      |  |                                    |  |     / ,    / ,  |.-'.-'\n" + //
+                        "      `>.|;, \\_)    \\_)     |  |  [4] | Volver al menú principal.   |  |    (_/    (_/ ,;|.<'\n" + //
+                        "       `-;     ,    /       |  |                                    |  |       \\    ,     ;-'\n" + //
+                        "          \\    /   <        |  |  [0] | Salir.                      |  |        >   \\    /\n" + //
+                        "           '. <`'-._)       |  |                                    |  |       (_,-'`> .'\n" + //
+                        "            '._)            |  | » » »      PokeChangua       « « « |  |           (_,'\n" + //
+                        "");
+    }
+
+    public static void menuIniciarBatalla(){
+
+    }
+
+    // > > > > > > > > > > > > > > > > > > > > - < < < < < < < < < < < < < < < < < < < < //
+    // > > > > > > > > > > > > > > > > > > O T R O S < < < < < < < < < < < < < < < < < < //
+    // > > > > > > > > > > > > > > > > > > > > - < < < < < < < < < < < < < < < < < < < < //
+
+    public static void espacioVisual(){
+        System.out.println(" \n \n \n \n \n \n \n");
+    }
+
+    public static void mostrarMensaje(String mensaje){
+        espacioVisual();
+        System.out.println(mensaje);
+        espacioVisual();
     }
     
+    public static void interfazPokeChangua(){
+        
+    }
+
+
+
 }
