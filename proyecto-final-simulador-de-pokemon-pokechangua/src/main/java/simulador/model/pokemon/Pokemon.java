5737194350f53c2f6747971f5b940b778576e01c
13rian Vargas <bsvargasc13@gmail.com>
2024-10-10 03:32:05 -0500

    DONE - Clase Pokémon completa.

--- proyecto-final-simulador-de-pokemon-pokechangua/src/main/java/simulador/model/pokemon/Pokemon.java
+++ proyecto-final-simulador-de-pokemon-pokechangua/src/main/java/simulador/model/pokemon/Pokemon.java
@@ -1,5 +1,88 @@
 package simulador.model.pokemon;
 
-public class Pokemon {
-    // TODO: Aquí va tu código
+import java.io.Serializable;
+
+public class Pokemon implements Serializable{
+    protected String nombre;
+    protected double salud;
+    protected double puntosDeAtaque;
+    protected TipoPokemon tipo;
+    protected Estado estado;
+
+    public Pokemon(String nombre, double salud, double puntosDeAtaque, TipoPokemon tipo) {
+        this.nombre = nombre;
+        this.salud = salud;
+        this.puntosDeAtaque = puntosDeAtaque;
+        this.tipo = tipo;
+        this.estado = Estado.NORMAL;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getSalud() {
+        return salud;
+    }
+
+    public void setSalud(double salud) {
+        this.salud = salud;
+    }
+
+    public double getPuntosDeAtaque() {
+        return puntosDeAtaque;
+    }
+
+    public void setPuntosDeAtaque(double puntosDeAtaque) {
+        this.puntosDeAtaque = puntosDeAtaque;
+    }
+
+    public TipoPokemon getTipo() {
+        return tipo;
+    }
+
+    /*/ <- Agregar * entre las barras para comentar método.
+    public void setTipo(TipoPokemon tipo) {
+        this.tipo = tipo;
+    }
+    //*/
+    public Estado getEstado() {
+        return estado;
+    }
+
+    /*/ <- Agregar * entre las barras para comentar método.
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+    //*/
+
+    //Complejidad temporal: O(1) Tiempo constante
+    public double atacar(Pokemon oponente) {
+        double multiplicador = TipoPokemon.obtenerMultiplicadorDeDaño(this.tipo, oponente.getTipo());
+        double daño = this.puntosDeAtaque * multiplicador;
+        return daño;
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante
+    public void recibirDaño(double daño) {
+        this.salud -= daño;
+        if (this.salud <= 0) {
+            this.salud = 0;
+            this.estado = Estado.DEBILITADO;
+            //TODO: Enviar mensaje que el pokémon ha sido debilitado.
+        }
+    }
+
+    //Complejidad temporal: O(1) Tiempo constante
+    public void entrenar() {
+        this.puntosDeAtaque += 20;
+        this.salud += 5;
+        //TODO: Enviar mensaje que el pokémon ha sido entrenado.
+    }
+    
+
 }
